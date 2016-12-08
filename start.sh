724deb90b8fe8cde0a9c30631aea0123d18f9e45
Hemant Sachdeva <hemant.sachdeva04@gmail.com>
2016-12-08 20:04:04 +0530

    test

---
+++ start.sh
@@ -0,0 +1,19 @@
+while getopts c:a:h: opts; do
+	case ${opts} in
+		c) 
+		CONFIGPKG=${OPTARG}
+		;;
+		a) APP=${OPTARG}
+		;;
+		h) ADD=${OPTARG}
+		;;
+	esac
+done
+
+echo 'Moving configuration package '
+echo $CONFIGPKG
+
+echo 'To remote location '
+eval HOST_ADD='$'$ADD
+echo $HOST_ADD
+
